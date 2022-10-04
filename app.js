09df0628ae41423e70d3b064830f73813653e640
ShravanJP <shravanjp8910@gmail.com>
2022-10-04 18:30:23 +0530

    Add routes

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.set('view engine','ejs');
+app.set('views','views');
+
+const adminData = require('./routes/admin');
+const userData = require('./routes/users');
+
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/admin', adminData.routes);
+app.use('/users',userData.routes);
+app.use('/',function(req,res,next){
+    res.status(404).render('404',{pageTitle:'404 Error'});
+})
+
 
 app.listen(3000);
